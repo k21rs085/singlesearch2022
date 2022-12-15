bfa0c8516c9b387c03f0219ffdd1fc62bc7a0f7a
k21rs085 <113566838+k21rs085@users.noreply.github.com>
2022-12-15 10:39:17 +0900

    Project's Initial Commit

---
+++ www/keys.js
@@ -0,0 +1,4 @@
+// This is a mBaaS keys file
+
+var appKey    = "20246b48645e83cc84a3e17e24c7f2627e48a512291d1e3eb30392e0d65e123e";
+var clientKey = "d8f0dd02c076372425cc3d5b31f2a5083c8f1c5c5a5e66ba40a1730269867967";
